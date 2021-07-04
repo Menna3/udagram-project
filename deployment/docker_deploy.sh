79406f426eff6edcd8d918aee3f359a52fbe312a
Menna Atef <menna.atef@silkbytes.com>
2021-07-04 18:32:56 +0200

    docker image name

--- deployment/docker_deploy.sh
+++ deployment/docker_deploy.sh
@@ -3,4 +3,4 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 docker push mennaatef/udagram-users-api
 docker push mennaatef/udagram-feeds-api
 docker push mennaatef/reverseproxy
-docker push mennaatef/udagram-frontend:local
+docker push mennaatef/udagram-frontend
