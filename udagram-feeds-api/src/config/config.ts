e987063381626864481488a56b6de194cf06e583
Menna Atef <menna.atef@silkbytes.com>
2021-07-04 15:37:41 +0200

    initial commit

---
+++ udagram-feeds-api/src/config/config.ts
@@ -0,0 +1,14 @@
+export const config = {
+  'username': 'postgres',
+  'password': 'mennamenna',
+  'database': 'postgres',
+  'host': 'postgres.ckebdgekfgig.us-east-1.rds.amazonaws.com',
+  'dialect': 'postgres',
+  'aws_region': 'us-east-1',
+  'aws_profile': 'default',
+  'aws_media_bucket': 'udagram-bucket-udacity',
+  'url': 'http://localhost:8080',
+  'jwt': {
+    'secret': 'hello',
+  },
+};
